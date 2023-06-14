c0a708f25a6efa118f9c785fc959b5c56dfdee33
Dillion Megida <dillionmegida@gmail.com>
2023-06-15 01:44:45 +0200

    fix tracking id and favicon image

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,21 +55,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `Deeecode`,
+        short_name: `Deeecode`,
         start_url: `/`,
         background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/banner.png`,
       },
     },
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: ["G-YNPDWE075X"],
+        trackingIds: ["G-24Q1PDJLE8"],
         gtagConfig: {
           head: true,
           anonymize_ip: true,
